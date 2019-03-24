813416294d8036f095690335351b21df5242f4f0
jpesina1 <jpesina1@mail.stmarytx.edu>
2019-03-24 16:29:38 -0500

    Add files via upload

---
+++ Transport.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transport : MonoBehaviour
+{
+
+    private bool isWatched = false;
+    private bool isStatic = false;
+    public int timePassed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Transportation());
+        if (isWatched == true) {
+            Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y +2,
+                                                                  Camera.main.transform.position.z);
+            isWatched = false;
+        }
+    }
+    IEnumerator Transportation() {
+        yield return new WaitForSeconds(timePassed);
+        isWatched = true;
+    }
+}
